f97c2db05a173764a2dd3097449a54ef69e660d4
sklaw <sk.law.lsq@gmail.com>
2018-01-30 16:56:12 +0800

    add RosbagRecord

---
+++ src/smach_based_introspection_framework/_experiment_recording_via_rosbag.py
@@ -0,0 +1,25 @@
+import subprocess
+import os
+import signal
+import psutil
+
+class RosbagRecord(object):
+    def __init__(self, bag_path, list_of_topics):
+        self.cmd = "rosbag record -O %s %s"%(os.path.realpath(bag_path), ' '.join(list_of_topics))
+        self.started = False
+
+    def start(self):
+        if self.started:
+            return
+        self.process = subprocess.Popen(self.cmd, shell=True)
+        self.started = True
+
+    def stop(self):
+        if not self.started:
+            return
+        ppid = self.process.pid 
+        parent = psutil.Process(ppid)
+        for child in parent.children(recursive=True):
+            child.kill()
+        parent.kill()
+        self.started = False 
