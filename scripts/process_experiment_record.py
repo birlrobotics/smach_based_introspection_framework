ae8313b3e60a90d89dc2a4729f8a26ca9126b490
sklaw <sk.law.lsq@gmail.com>
2018-02-01 21:04:28 +0800

    saving

--- scripts/process_experiment_record.py
+++ scripts/process_experiment_record.py
@@ -9,6 +9,9 @@ from birl_offline_data_handler.rosbag_handler import (
 from smach_based_introspection_framework.experiment_record_processor import (
     get_tag_range,
 )
+from birl_offline_data_handler.rosbag_anomaly_extractor import (
+    RosbagAnomalyExtractor
+)
 
 if __name__ == "__main__":
     from smach_based_introspection_framework._constant import introspection_data_folder
@@ -20,10 +23,27 @@ if __name__ == "__main__":
 
     for exp_dir in exp_dirs: 
         print exp_dir
-        rh = RosbagHandler(os.path.join(exp_dir, "record.bag"))
-        l = rh.get_csv_of_a_topic("/tag_multimodal") 
-        if len(l) == 0:
+        try:
+            rh = RosbagHandler(os.path.join(exp_dir, "record.bag"))
+        except InvalidRosbagPath as e:
             raise Exception("Can't find record.bag in %s"%exp_dir) 
+        
+        l = rh.get_csv_of_a_topic("/tag_multimodal") 
+        if len(l) != 1:
+            raise Exception("Failed to get /tag_multimodal from record.bag in %s"%exp_dir)
         bag_path, tag_df = l[0]
         list_of_tag_range = get_tag_range(tag_df)
         print list_of_tag_range
+
+
+        rae = RosbagAnomalyExtractor(os.path.join(exp_dir, "record.bag"))
+        ret = rae.get_anomaly_csv(
+            "/tag_multimodal",
+            "/anomaly_detection_signal",
+            4,
+            10,
+        )
+        for i in ret:
+            print i[0]
+            for j in i[1]:
+                print j[0]
