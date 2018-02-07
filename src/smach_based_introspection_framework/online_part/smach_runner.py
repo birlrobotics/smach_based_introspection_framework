7f07bec2ed8d09915f7c3c58b776bb7c86660593
sklaw <sk.law.lsq@gmail.com>
2018-02-07 20:00:09 +0800

    implementing smach_runner

--- src/smach_based_introspection_framework/online_part/smach_runner.py
+++ src/smach_based_introspection_framework/online_part/smach_runner.py
@@ -1,30 +1,61 @@
 from smach_modifier import modify_user_sm
-import data_collection.main
 import rospy
 import os
 import smach_ros
+from std_srvs.srv import SetBool, SetBoolRequest
 
 def shutdown():
     rospy.loginfo("Shuting down, PID: %s"%os.getpid())
-    data_collection.main.stop() 
     pass
+
+def control_service_processes(start):
+    snames = [
+        'toggle_anomaly_classification_process_service',
+        'toggle_rosbag_process_service',
+        'toggle_tag_multimodal_topic_process_service',     
+    ]
+    if start:
+        req = SetBoolRequest()
+        req.data = True
+        for sname in snames:
+            sp = rospy.ServiceProxy(sname, SetBool)
+            resp = sp(req)
+            if not resp.success:
+                raise Exception("Failed to start process using service: %s. Errmsg: %s"%(snamei, resp.message))
+    else:
+        req = SetBoolRequest()
+        req.data = False
+        for sname in snames:
+            sp = rospy.ServiceProxy(sname, SetBool)
+            resp = sp(req)
 
 def run(sm):
     try: 
         rospy.loginfo("PID: %s"%os.getpid())
         rospy.init_node("smach_based_introspection_framework_node")
         rospy.on_shutdown(shutdown)
-        sm = modify_user_sm.run(sm)
 
         sis = smach_ros.IntrospectionServer('MY_SERVER', sm, '/SM_ROOT')
         sis.start()
+        sm = modify_user_sm.run(sm)
 
-        data_collection.main.start()
-        rospy.loginfo("Gonna wait 5 secs after starting online_data_collection")
+        try:
+            control_service_processes(True)
+        except Exception as e:
+            control_service_processes(False)
+            rospy.loginfo(str(e))
+            raise
+        rospy.loginfo("service processes up, start sm in 5 secs.")
         rospy.sleep(5)
 
-        #outcome = sm.execute()
-        #print 'sm.execute() returns', outcome
+        rospy.loginfo("start sm.")
+        outcome = sm.execute()
+        rospy.loginfo('sm.execute() returns %s'%outcome)
+
+        rospy.loginfo("tear down service processes in 5 secs.")
+        rospy.sleep(5)
+        control_service_processes(False)
+        rospy.loginfo("teared down.")
 
         sis.stop()
     except:
