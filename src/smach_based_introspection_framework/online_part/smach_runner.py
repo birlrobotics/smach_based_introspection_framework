70956ca5d5611e4889dd8874afa9685666f21549
sklaw <sk.law.lsq@gmail.com>
2018-02-07 23:37:16 +0800

    saving

--- src/smach_based_introspection_framework/online_part/smach_runner.py
+++ src/smach_based_introspection_framework/online_part/smach_runner.py
@@ -3,31 +3,62 @@ import rospy
 import os
 import smach_ros
 from std_srvs.srv import SetBool, SetBoolRequest
+from smach_based_introspection_framework._constant import (
+    latest_experiment_record_folder,
+    experiment_record_folder, 
+    folder_time_fmt,
+)
+from smach_based_introspection_framework.online_part.process_runner.rosbag_process import (
+    RosbagProc
+)
+from smach_based_introspection_framework.online_part.process_runner.anomaly_classification_process import (
+    AnomalyClassificationProc 
+)
+from smach_based_introspection_framework.online_part.process_runner.tag_multimodal_topic_process import (
+   TagMultimodalTopicProc, 
+)
+import shutil
+import datetime
 
 def shutdown():
     rospy.loginfo("Shuting down, PID: %s"%os.getpid())
     pass
 
-def control_service_processes(start):
-    snames = [
-        'toggle_anomaly_classification_process_service',
-        'toggle_rosbag_process_service',
-        'toggle_tag_multimodal_topic_process_service',     
-    ]
+rosbag_proc = None
+ac_proc = None
+tmt_proc = None
+sis = None
+def toggle_introspection(start):
+    global rosbag_proc, ac_proc, tmt_proc, sis
     if start:
-        req = SetBoolRequest()
-        req.data = True
-        for sname in snames:
-            sp = rospy.ServiceProxy(sname, SetBool)
-            resp = sp(req)
-            if not resp.success:
-                raise Exception("Failed to start process using service: %s. Errmsg: %s"%(snamei, resp.message))
+        if not os.path.isdir(latest_experiment_record_folder):
+            os.makedirs(latest_experiment_record_folder)
+        rosbag_proc = RosbagProc(
+            os.path.join(latest_experiment_record_folder, "record.bag"),
+            ['/tag_multimodal', '/anomaly_detection_signal']
+        )
+        rosbag_proc.start()
+        ac_proc = AnomalyClassificationProc()
+        ac_proc.start()
+        tmt_proc = TagMultimodalTopicProc()
+        tmt_proc.start()
+        sis = smach_ros.IntrospectionServer('MY_SERVER', sm, '/SM_ROOT')
+        sis.start()
+        rospy.sleep(2)
     else:
-        req = SetBoolRequest()
-        req.data = False
-        for sname in snames:
-            sp = rospy.ServiceProxy(sname, SetBool)
-            resp = sp(req)
+        rospy.sleep(2)
+        rosbag_proc.stop()
+        ac_proc.stop()
+        tmt_proc.stop()
+        sis.stop()
+        rospy.sleep(2)
+        shutil.move(
+            latest_experiment_dir, 
+            os.path.join(
+                experiment_record_folder,
+                'experiment_at_%s'%datetime.datetime.now().strftime(folder_time_fmt),
+            )
+        )
 
 def run(sm):
     try: 
@@ -35,28 +66,22 @@ def run(sm):
         rospy.init_node("smach_based_introspection_framework_node")
         rospy.on_shutdown(shutdown)
 
-        sis = smach_ros.IntrospectionServer('MY_SERVER', sm, '/SM_ROOT')
-        sis.start()
         sm = modify_user_sm.run(sm)
 
         try:
-            control_service_processes(True)
+            toggle_introspection(True)
         except Exception as e:
-            control_service_processes(False)
+            toggle_introspection(False)
             rospy.loginfo(str(e))
             raise
-        rospy.loginfo("service processes up, start sm in 5 secs.")
-        rospy.sleep(5)
+        rospy.loginfo("introspection up.")
 
         rospy.loginfo("start sm.")
         outcome = sm.execute()
         rospy.loginfo('sm.execute() returns %s'%outcome)
 
-        rospy.loginfo("tear down service processes in 5 secs.")
-        rospy.sleep(5)
-        control_service_processes(False)
-        rospy.loginfo("teared down.")
+        toggle_introspection(False)
+        rospy.loginfo("introspection down.")
 
-        sis.stop()
     except:
         os.killpg(os.getpid())
