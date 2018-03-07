b40451dd788647e084a65d286a26ab91687972f1
sklaw <sk.law.lsq@gmail.com>
2018-03-07 08:31:07 +0800

    Test bnpy hmm incremental calculator

--- src/smach_based_introspection_framework/online_part/anomaly_detector/log_likelihood_incremental_calculator.py
+++ src/smach_based_introspection_framework/online_part/anomaly_detector/log_likelihood_incremental_calculator.py
@@ -1,5 +1,7 @@
 import numpy as np
+
 from scipy.misc import logsumexp
+import ipdb
 
 def log_mask_zero(a):
     """Computes the log of input probabilities masking divide by zero in log.
@@ -43,32 +45,39 @@ class HmmlearnModelIncrementalLoglikCalculator(object):
 
 class BNPYModelIncrementalLoglikCalculator(object):
     def __init__(self, model):
+        model = model.model
         self.model = model
-        self.n_components = model.K
+        self.n_components = model.allocModel.K
         self.log_transmat = log_mask_zero(model.allocModel.get_init_prob_vector()) 
         self.log_startprob = log_mask_zero(model.allocModel.get_trans_prob_matrix())
         self.fwdlattice = None
+        self.preSample = None
         self.work_buffer = np.zeros(self.n_components)
 
     def add_one_sample_and_get_loglik(self, sample):
-        if np.array([sample]).shape[0] == 1:
-            sample = np.append([sample],[sample], axis=0)
-        Xprev  = sample[:-1,:]
-        X      = sample[1:,:]
-        length = len(X)
+        ipdb.set_trace()
+        import bnpy
+        if self.preSample is None:
+            self.preSample = sample
+            return 0
+        else:
+            Xprev = self.preSample
+            X = sample
+            self.preSample = sample
+        length = 1
         doc_range = [0, length]
         dataset = bnpy.data.GroupXData(X, doc_range, length, Xprev)
+
         LP = self.model.calc_local_params(dataset)
         framelogprob = LP['E_log_soft_ev'] 
         if self.fwdlattice is None:
-            self.fwdlattice = np.zeros((1, self.K))
-            for i in range(self.K):
+            self.fwdlattice = np.zeros((1, self.n_components))
+            for i in range(self.n_components):
                 self.fwdlattice[0,i] = self.log_startprob[i] + framelogprob[0,i]
-            print "I am here"
         else:
-            self.fwdlattice = np.append(self.fwdlattice, np.zeros((1, self.K)), axis=0)
-            for j in range(self.K):
-                for i in range(self.K):
+            self.fwdlattice = np.append(self.fwdlattice, np.zeros((1, self.n_components)), axis=0)
+            for j in range(self.n_components):
+                for i in range(self.n_components):
                     self.work_buffer[i] = self.fwdlattice[-2,i] + self.log_transmat[i,j]
                 self.fwdlattice[-1,j] = logsumexp(self.work_buffer) + framelogprob[0,j]
         curr_log = logsumexp(self.fwdlattice[-1])
@@ -94,3 +103,12 @@ def get_calculator(model):
     else:
         print "Returning BasicCalculator! HMM incremental calculation will NOT be optimal."
         return BasicCalculator(model)
+
+
+if __name__ == '__main__':
+    from sklearn.externals import joblib
+    import numpy
+    model = joblib.load('test_data/introspection_model')['hmm_model']
+    c = get_calculator(model) 
+    while True:
+        c.add_one_sample_and_get_loglik(numpy.array([[1]*18]))
