decbdd85e117b2fcdf297f91ea624f2da8ae0e3e
sklaw <sk.law.lsq@gmail.com>
2018-04-23 22:46:19 +0800

    Add twist filter

--- src/smach_based_introspection_framework/offline_part/anomaly_detection_feature_selection/filtering_schemes.py
+++ src/smach_based_introspection_framework/offline_part/anomaly_detection_feature_selection/filtering_schemes.py
@@ -1,8 +1,10 @@
 from geometry_msgs.msg import WrenchStamped
+from baxter_core_msgs.msg import EndpointState 
 import msg_filters
 from rostopics_to_timeseries import RosTopicFilteringScheme
 
 filtering_schemes = []
+
 
 tfc = RosTopicFilteringScheme()
 tfc.add_filter(
@@ -12,10 +14,34 @@ tfc.add_filter(
 )
 filtering_schemes.append(tfc)
 
+
 tfc = RosTopicFilteringScheme()
 tfc.add_filter(
     "/robotiq_force_torque_wrench", 
     WrenchStamped, 
     msg_filters.WrenchStampedNormFilter(),
+)
+filtering_schemes.append(tfc)
+
+
+tfc = RosTopicFilteringScheme()
+tfc.add_filter(
+    "/robotiq_force_torque_wrench", 
+    WrenchStamped, 
+    msg_filters.WrenchStampedNormFilter(),
+)
+tfc.add_filter(
+    "/robot/limb/right/endpoint_state", 
+    EndpointState,
+    msg_filters.BaxterEndpointTwistNormFilter(),
+)
+filtering_schemes.append(tfc)
+
+
+tfc = RosTopicFilteringScheme()
+tfc.add_filter(
+    "/robot/limb/right/endpoint_state", 
+    EndpointState,
+    msg_filters.BaxterEndpointTwistNormFilter(),
 )
 filtering_schemes.append(tfc)
--- src/smach_based_introspection_framework/offline_part/anomaly_detection_feature_selection/msg_filters.py
+++ src/smach_based_introspection_framework/offline_part/anomaly_detection_feature_selection/msg_filters.py
@@ -42,3 +42,28 @@ class WrenchStampedNormFilter(TopicMsgFilter):
             'robotiq_force_sensor.wrench.force.norm', \
             'robotiq_force_sensor.wrench.torque.norm',\
         ] 
+
+class BaxterEndpointTwistNormFilter(TopicMsgFilter):
+    def __init__(self):
+        super(BaxterEndpointTwistNormFilter, self).__init__()
+
+    def convert(self, msg):
+        linear_norm = np.linalg.norm([
+            msg.twist.linear.x,\
+            msg.twist.linear.y,\
+            msg.twist.linear.z,\
+        ])
+        angular_norm = np.linalg.norm([
+            msg.twist.angular.x,\
+            msg.twist.angular.y,\
+            msg.twist.angular.z,\
+        ])
+        return [linear_norm, angular_norm] 
+    def vector_size(self):
+        return 2 
+
+    def vector_meaning(self):
+        return [
+            'baxter_enpoint_pose.twist.linear.norm', \
+            'baxter_enpoint_pose.twist.angular.norm', \
+        ] 
