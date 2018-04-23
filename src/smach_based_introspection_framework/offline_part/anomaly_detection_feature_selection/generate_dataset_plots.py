81c490c52c6b9eb42a1594b3552708e0946016fe
sklaw <sk.law.lsq@gmail.com>
2018-04-23 17:21:33 +0800

    Add visualization script

---
+++ src/smach_based_introspection_framework/offline_part/anomaly_detection_feature_selection/generate_dataset_plots.py
@@ -0,0 +1,69 @@
+
+import itertools
+from smach_based_introspection_framework._constant import (
+    datasets_of_filtering_schemes_folder,
+)
+import glob
+import os
+import pandas as pd
+import coloredlogs, logging
+import matplotlib.pyplot as plt
+
+def run():
+    logger = logging.getLogger('GenPlots')
+    logger.setLevel(logging.INFO)
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setLevel(logging.INFO)
+    logger.addHandler(consoleHandler)
+
+    succ_folders = glob.glob(os.path.join(
+        datasets_of_filtering_schemes_folder,
+        'No.* filtering scheme',
+        'successful_skills',
+        'skill *',
+    )) 
+
+    unsucc_folders = glob.glob(os.path.join(
+        datasets_of_filtering_schemes_folder,
+        'No.* filtering scheme',
+        'unsuccessful_skills',
+        'skill *',
+    )) 
+
+    for folder in itertools.chain(succ_folders, unsucc_folders):
+        logger.info(folder)
+        path_postfix = os.path.relpath(folder, datasets_of_filtering_schemes_folder)
+
+        output_dir = os.path.join(datasets_of_filtering_schemes_folder, "visualized dataset", path_postfix)
+
+        xs = None
+        args = {}
+        for csv in glob.glob(os.path.join(folder, '*', '*.csv')):
+            logger.info(csv)
+            df = pd.read_csv(csv)
+            for idx, name in enumerate(df.columns):
+                if idx == 0:
+                    xs = df.iloc[:, 0]
+                    xs = xs-xs[0]
+                else:
+                    if name not in args:
+                        args[name] = []
+                    args[name].append(xs)
+                    args[name].append(df.iloc[:, idx])
+
+        for key, value in args.iteritems():
+            fig, ax = plt.subplots(nrows=1, ncols=1)
+            ax.plot(*value)
+            ax.set_title(path_postfix+'\n'+key)
+    
+            fig_file = os.path.join(output_dir, "featuer %s.png"%key)
+
+            if not os.path.isdir(output_dir):
+                os.makedirs(output_dir)
+            ax.set_xlim(right=min(ax.get_xlim()[1], 20))
+            fig.set_size_inches(16, 16)
+            fig.savefig(fig_file)
+
+if __name__ == '__main__':
+    run()
+    
