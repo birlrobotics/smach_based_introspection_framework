3a619aa20676c0adfdec09547b57ac7ef4bfd253
sklaw <sk.law.lsq@gmail.com>
2018-05-08 15:18:23 +0800

    Use new rtts API

--- src/smach_based_introspection_framework/online_part/process_runner/scripts/timeseries_publisher.py
+++ src/smach_based_introspection_framework/online_part/process_runner/scripts/timeseries_publisher.py
@@ -2,14 +2,13 @@ from rostopics_to_timeseries.RostopicsToTimeseries import OnlineRostopicsToTimes
 import rospy
 from smach_based_introspection_framework.configurables import (
     tfc,
-    timeseries_rate,
 )
 
 if __name__ == '__main__':
     rospy.init_node("timeseries_publisher_node") # Needed to init a ROS node first
     rospy.loginfo("timeseries_publisher.py starts")
     # Pass in topic configuration and timeseries rate
-    onrt = OnlineRostopicsToTimeseries(tfc, rate=timeseries_rate) 
+    onrt = OnlineRostopicsToTimeseries(tfc) 
     # Pass in the topic name that publishes the timeseries vector
     onrt.start_publishing_timeseries("/rostopics_to_timeseries_topic") 
     rospy.loginfo("timeseries_publisher.py exits")
