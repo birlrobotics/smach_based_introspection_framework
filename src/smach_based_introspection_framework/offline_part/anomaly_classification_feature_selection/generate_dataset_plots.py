a3e4afcb57940dd17b86b7ae2ba2f807a9a75a78
sklaw <sk.law.lsq@gmail.com>
2018-06-02 16:38:34 +0800

    generate_dataset_plots.py: add the function to plot the whole anomaly data by atype in one figure

--- src/smach_based_introspection_framework/offline_part/anomaly_classification_feature_selection/generate_dataset_plots.py
+++ src/smach_based_introspection_framework/offline_part/anomaly_classification_feature_selection/generate_dataset_plots.py
@@ -24,7 +24,7 @@ def run():
     logger.info(folders)
 
     at_extractor = re.compile(r'anomaly_type_\((.*)\)')
-
+    
     for folder in itertools.chain(folders):
         logger.info(folder)
         path_postfix = os.path.relpath(folder, anomaly_classification_feature_selection_folder)
@@ -32,7 +32,10 @@ def run():
         anomaly_type = at_extractor.search(path_postfix).group(1)
 
         output_dir = os.path.join(anomaly_classification_feature_selection_folder, "visualized dataset", path_postfix)
-        for csv in glob.glob(os.path.join(folder, '*', '*.csv')):
+        csv_files = glob.glob(os.path.join(folder, '*', '*.csv'))
+        num_by_type = len(csv_files)
+        fig_whole_type, axarr = plt.subplots(nrows=num_by_type, ncols=1)        
+        for num, csv in enumerate(glob.glob(os.path.join(folder, '*', '*.csv'))):
     
             logger.info(csv)
             exp_name = os.path.basename(csv)
@@ -52,6 +55,16 @@ def run():
                     xs = df.iloc[:, 0]
                     df.plot(ax=ax, x=df.keys()[0],y=df.keys()[1::])
                     ax.legend(loc=2, bbox_to_anchor=(1.05, 1), fancybox=True, shadow=True, ncol=1)
+                    if num_by_type <= 1:
+                        df.plot(ax=axarr, x=df.keys()[0], y=df.keys()[1::], title=exp_name , legend=False)
+                        axarr.axvline(anomaly_t, c='purple')
+                        ymin,ymax = axarr.get_ylim()
+                        axarr.text(anomaly_t, ymax-0.25*(ymax-ymin), '%s'%anomaly_type, color='purple', rotation=-90)                           
+                    else:
+                        df.plot(ax=axarr[num], x=df.keys()[0], y=df.keys()[1::], title=exp_name, legend=False)
+                        axarr[num].axvline(anomaly_t, c='purple')
+                        ymin,ymax = axarr[num].get_ylim()
+                        axarr[num].text(anomaly_t, ymax-0.25*(ymax-ymin), '%s'%anomaly_type, color='purple', rotation=-90)   
                 else:
                     ys = df.iloc[:, idx]
                     ax.plot(xs, ys)                    
@@ -66,6 +79,9 @@ def run():
                 with open(output_f, 'w') as f:
                     fig.savefig(f)
                 plt.close(fig)
+        save_path = os.path.join(output_dir, anomaly_type + '.png')
+        fig_whole_type.savefig(save_path, dpi=300)
+        
 
 if __name__ == '__main__':
     run()
