1fe6ff805597abb09977081dff78a040952e127f
sklaw <sk.law.lsq@gmail.com>
2018-06-14 09:10:13 +0800

    add the 2D tactile sensor std with scaling and clipping

--- src/smach_based_introspection_framework/offline_part/anomaly_detection_feature_selection/msg_filters_with_scaling_and_clip.py
+++ src/smach_based_introspection_framework/offline_part/anomaly_detection_feature_selection/msg_filters_with_scaling_and_clip.py
@@ -24,3 +24,28 @@ class TactileStaticStdScaleClipMaxFilter(TopicMsgFilter):
         ] 
 
 
+class TactileStaticStdScaleClipFilter(TopicMsgFilter):
+    def __init__(self):
+        super(TactileStaticStdScaleClipFilter, self).__init__()
+
+    def convert(self, msg):
+        ret = np.array([
+            np.std(msg.taxels[0].values),
+            np.std(msg.taxels[1].values),
+        ])
+        return np.clip(ret/60.0, -1, 1)
+        
+
+    @staticmethod
+    def vector_size():
+        return 2
+
+    @staticmethod
+    def vector_meaning():
+        return [
+            'tactile_static_data.left.std.clip(ret/60.0, -1, 1)', \
+            'tactile_static_data.right.std.clip(ret/60.0, -1, 1)', \
+        ] 
+
+        
+
